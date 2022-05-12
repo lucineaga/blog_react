23a447ee0863c68e7a922e3547ee343688a3ddb0
lucineaga <neagalucian2@gmail.com>
2022-05-12 20:22:40 +0300

    first save

---
+++ src/views/Home.js
@@ -0,0 +1,18 @@
+import { Container } from "reactstrap";
+
+function Home() {
+	return (
+		<Container>
+			<Row>
+				<div
+					style={{
+						height: "100vh",
+						width: "100%",
+						backgroundColor: "gray",
+					}}></div>
+			</Row>
+		</Container>
+	);
+}
+
+export default Home;
---
+++ src/views/Wishlist.js
@@ -0,0 +1,5 @@
+function Wishlist() {
+	return <h1> Pagina Wishlist</h1>;
+}
+
+export default Wishlist;
